777a67dbe1e498ea2f5d5acec57c31b26389fcf7
Nicolas <nquesada@albany.edu>
2024-10-21 17:04:02 -0400

    s

--- backend/models/Login.js
+++ backend/models/Login.js
@@ -10,30 +10,38 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 const userTable = 'geosecure-users';
 
 async function login(userInfo) {
-    const username = userInfo.username
-    const password = userInfo.password
-
-    if(!userInfo || !username || !password){
+    const username = userInfo.username;
+    const password = userInfo.password;
+    
+    if (!userInfo || !username || !password) {
         return util.buildResponse(401, {
-            message: 'username and password are required'
-        })
+            message: 'Username and password are required'
+        });
     }
+
     const dynamoUser = await getUser(username.toLowerCase().trim());
-    if(!dynamoUser || !dynamoUser.username){
-        return util.buildResponse(403, { message: 'user does not exist' });
+    if (!dynamoUser || !dynamoUser.username) {
+        return util.buildResponse(403, { message: 'User does not exist' });
     }
-    if(!bcrypt.compareSync(password, dynamoUser.password)){
-        return util.buildResponse(403, {message: 'password is incorrect'})
+
+    if (!bcrypt.compareSync(password, dynamoUser.password)) {
+        return util.buildResponse(403, { message: 'Password is incorrect' });
     }
-    const userInfo = {
+
+    // Do not redeclare userInfo here
+    const token = auth.generateToken({
         username: dynamoUser.username,
         name: dynamoUser.name
-    }
-    const token = auth.generateToken(userInfo)
+    });
+
     const response = {
-        user: userInfo,
+        user: {
+            username: dynamoUser.username,
+            name: dynamoUser.name
+        },
         token: token
-    }
+    };
+    
     return util.buildResponse(200, response);
 }
 
