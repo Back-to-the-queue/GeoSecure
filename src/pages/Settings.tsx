81af0e9a43fba9b8d6a0a3aa1520a9b8dbfbd82e
Matt Mastrangelo <mmastrangelo2@albany.edu>
2024-11-04 11:04:08 -0500

    Basic Settings Page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,7 +1,9 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonAlert, IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonMenuButton, IonPage, IonText, IonTitle, IonToggle, IonToolbar } from '@ionic/react';
+import { chevronForwardCircleOutline, fingerPrintOutline, notificationsOutline, personOutline } from 'ionicons/icons';
+import React, { useState } from 'react';
 
 const Settings: React.FC = () => {
+    const [showAlert, setShowAlert] = useState(false);
 
     return (
         <IonPage>
@@ -14,9 +16,47 @@ const Settings: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent className="ion-padding" fullscreen>
-                <div style={{ textAlign: 'center'}}>
-                    <h1>Coming Soon!</h1>
-                </div>
+            <IonItem>
+                <IonIcon icon={fingerPrintOutline} />
+                <IonLabel>
+                    Anonymous Data Sharing
+                    <IonText color="medium" style={{ display: 'block', fontSize: 'small' }}>
+                        Enable to share data anonymously to improve our service.
+                    </IonText>
+                </IonLabel>
+                <IonToggle slot="end"></IonToggle>
+            </IonItem>
+            <IonItem>
+                <IonIcon icon={notificationsOutline} />
+                <IonLabel>
+                    Real-Time Alerts
+                    <IonText color="medium" style={{ display: 'block', fontSize: 'small' }}>
+                        Receive immediate notifications.
+                    </IonText>
+                </IonLabel>
+                <IonToggle slot="end"></IonToggle>
+            </IonItem>
+            <IonItem>
+                    <IonIcon icon={personOutline} />
+                    <IonLabel>
+                        Adjust Privacy Settings
+                        <IonText color="medium" style={{ display: 'block', fontSize: 'small' }}>
+                            Customize privacy preferences for your account.
+                        </IonText>
+                    </IonLabel>
+                    <IonIcon
+                        icon={chevronForwardCircleOutline}
+                        slot="end"
+                        onClick={() => setShowAlert(true)}
+                    />
+                </IonItem>
+                <IonAlert
+                    isOpen={showAlert}
+                    onDidDismiss={() => setShowAlert(false)}
+                    header={'Error'}
+                    message={'Currently not available'}
+                    buttons={['OK']}
+                />
             </IonContent>
         </IonPage>
     );
